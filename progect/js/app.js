c17f98ceb0fd67bdf96cef9eec35d70d1405cf0b
daviddev <david.arutyunyan.@bk.ru>
2022-07-16 00:17:04 +0300

    css header part3

---
+++ progect/js/app.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 16,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop:'true'
+});
