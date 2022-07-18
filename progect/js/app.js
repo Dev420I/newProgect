a2eddb6259a4eda0f4cdfb22be2939ad34d3d6f1
daviddev <david.arutyunyan.@bk.ru>
2022-07-18 03:28:58 +0300

    css header, img part3.2

--- progect/js/app.js
+++ progect/js/app.js
@@ -1,27 +1,6 @@
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*=========== HOME SWIPER=======*/
 
 let homeSwiper = new Swiper(".home-swiper", {
     spaceBetween: 16,
@@ -29,3 +8,24 @@ let homeSwiper = new Swiper(".home-swiper", {
     slidesPerView: 'auto',
     loop:'true'
 });
+
+/*=========== SERVICES=======*/
+const tabs = document.querySelectorAll('[data-target]'),
+       tabContent = document.querySelectorAll('[data-content]')
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContent.forEach(tabContents => {
+            tabContents.classList.remove('services__active')
+        })
+
+        target.classList.add('services__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('services__active')
+        })
+
+        tab.classList.add('services__active')
+    })
+})
