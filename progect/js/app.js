67756d1c07194d0d9d60f955503a65d318b9c3ac
daviddev <david.arutyunyan.@bk.ru>
2022-07-21 01:42:46 +0300

    patch 5.1 adaptive html css js

--- progect/js/app.js
+++ progect/js/app.js
@@ -1,19 +1,45 @@
 
+/*=========== SHOW MENU=======*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+if (navToggle){
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu')
+        })
+    }
+
+
+
+
+if (navClose){
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu')
+        })
+    }
+
+
+/*=========== SCROLL HEADER=======*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=========== SCROLL HEADER=======*/
+
 function scrollHeader(){
     const header = document.getElementById('header')
     if (this.scrollY >= 50) header.classList.add('scroll-header');
     else header.classList.remove('scroll-header')
 }
-
 window.addEventListener('scroll', scrollHeader)
-
-
-
-
-
-
-
-
 
 
 /*=========== HOME SWIPER=======*/
