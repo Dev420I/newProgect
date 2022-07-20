14e8638da71b27a8a5a875f96b68e027ef26ea2a
daviddev <david.arutyunyan.@bk.ru>
2022-07-20 22:51:30 +0300

    css header, img part4.1

--- progect/js/app.js
+++ progect/js/app.js
@@ -1,4 +1,20 @@
 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+
+
+
+
+
+
+
+
 
 /*=========== HOME SWIPER=======*/
 
